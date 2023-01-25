42844c2d6c3f32c250fe600d697ddd6d88632237
harinipalani <112462896+harinipalani@users.noreply.github.com>
2023-01-25 14:12:39 +0530

    do while.c
    write a program for natural numbers in do while

---
+++ natural numbers in  do while.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int i,n;
+	printf("enter the value of n\t");
+	scanf("%d",&n);
+	printf("printing natural numbers from 1 to %d\n",n);
+	i=1;
+	do{
+		printf("%d\t",i);
+		i++;
+	}
+	while(i<=n);
+	printf("\n");
+}
